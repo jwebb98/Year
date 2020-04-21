0d0d04edf429b728f7e21b1e1713c33056128749
jwebb98 <webb-j5@ulster.ac.uk>
2020-04-21 22:56:29 +0100

    8th commit
    Category Select base has been completed and working

---
+++ app/src/main/java/com/project/Year/SliderAdapter.java
@@ -0,0 +1,83 @@
+package com.project.Year;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context){
+        this.context = context;
+    }
+
+    public int[] slide_images= {
+          R.drawable.common_google_signin_btn_icon_light_focused,
+          R.drawable.film,
+          R.drawable.history,
+          R.drawable.music,
+          R.drawable.film
+    };
+
+    public String[] slide_headings = {
+            "test","Film", "History", "Music", "Sports"
+    };
+
+    public String[] slide_descs = {
+      "test","random stuff 1 hopefully working FILM", "random stuff 2 hopefully working HISTORY", "random stuff 3 hopefully working MUSIC", "random stuff 4 hopefully working Film"
+    };
+
+   // public String[] slide_buttons = {
+    //        "Film Posters", "Historical Events", "Iconic Album Covers", "UEFA CL Teams"
+   // };
+
+    @Override
+    public int getCount() {
+        return slide_headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == (ConstraintLayout) o;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position){
+
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        ImageView slideImageView = (ImageView) view.findViewById(R.id.slide_image);
+        TextView slideHeading = (TextView) view.findViewById(R.id.slide_heading);
+        TextView slideDescription = (TextView) view.findViewById(R.id.slide_desc);
+        //Button slideButton = (Button) view.findViewById(R.id.slide_button);
+
+
+        slideImageView.setImageResource(slide_images[position]);
+        slideHeading.setText(slide_headings[position]);
+        slideDescription.setText(slide_descs[position]);
+        //slideButton.setText(slide_buttons[position]);
+
+
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object){
+        container.removeView((ConstraintLayout)object);
+    }
+
+
+}
